aefa46ff849d785c5f318c98f9b2e1c2c1cbbf73
Kevin Kariuki <vingentz@gmail.com>
2023-06-08 22:58:46 +0300

    Prints Magic Calculator

--- 0x02-python-import_modules/102-magic_calculator.py
+++ 0x02-python-import_modules/102-magic_calculator.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
+def magic_calculation(x, y):
     from magic_calculation_102 import add, sub
-    if a < b:
-        c = add(a, b)
+    if x < y:
+        z = add(x, y)
         for i in range(4, 6):
-            c = add(c, i)
+            z = add(c, i)
         return c
     else:
-        return sub(a, b)
+        return sub(x, y)
