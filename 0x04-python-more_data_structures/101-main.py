1fdfaa18d1d5226c30f6f1634e31da25cd092baf
Kevin Kariuki <vingentz@gmail.com>
2023-06-14 16:30:01 +0300

    Added 4 advanced main.py files

--- 0x04-python-more_data_structures/101-main.py
+++ 0x04-python-more_data_structures/101-main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
+square_matrix_map = \
+            __import__('101-square_matrix_map').square_matrix_map
 
 matrix = [
         [1, 2, 3],
@@ -7,6 +8,6 @@ matrix = [
         [7, 8, 9]
         ]
 
-new_matrix = square_matrix_simple(matrix)
+new_matrix = square_matrix_map(matrix)
 print(new_matrix)
 print(matrix)
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,4 +3,4 @@ def uniq_add(my_list=[]):
     k = 0
     for l in set(my_list):
         k += l
-    return k
+    return (k)
