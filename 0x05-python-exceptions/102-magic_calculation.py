80795b3c8d31e544b07345a0b8df5c45355365ba
Kevin Kariuki <vingentz@gmail.com>
2023-06-26 18:01:10 +0300

    Bytecode-> Python #4

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    final = 0
+    for k in range(1, 3):
+        try:
+            if k > a:
+                raise Exception("Too far")
+            else:
+                final += (a**b)/k
+        except:
+            final = b + a
+            break
+        return final
