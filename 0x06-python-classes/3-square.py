766d758acb0bd14db6a3646933707ebb19f25a3f
Kevin Kariuki <vingentz@gmail.com>
2023-06-28 14:58:12 +0300

    Creation of an class square to define a 2-square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Defines class Square with private attribute size'''
+
+
+class Square:
+    '''class sqaure definition with attribute size'''
+    def __init__(self, size=0):
+        '''initialize the size of a side of square'''
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        '''calculate current square area'''
+        return self.__size * self.__size
