325a0bd31397e3caaa27123dc8daa73147f72085
Kevin Kariuki <vingentz@gmail.com>
2023-07-03 14:33:04 +0300

    Class to define a rectangle print format

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,7 +50,7 @@ class Rectangle:
 
     def __str__(self):
         """Set print behaviour"""
-        if self.__width == 0 and self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return ""
         prints = "\n".join(["#" * self.__width for rows in range(self.__height)])
         return prints
