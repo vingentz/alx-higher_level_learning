70dc0d918a949d66b6bba7d3af9c5fd255a840b3
Kevin Kariuki <vingentz@gmail.com>
2023-07-03 16:03:44 +0300

    Class to define a rectangle recreation,deletion and numbering of

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,9 +5,13 @@
 
 class Rectangle:
     """class rectangle"""
+
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
