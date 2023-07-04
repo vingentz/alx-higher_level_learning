42b6b3d7dc2f61e9a4a8e71837b81b73c9176e45
Kevin Kariuki <vingentz@gmail.com>
2023-07-04 12:45:37 +0300

    Addition of all test files

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+""" Unittest for the max_integer module"""
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """ Test for max_integer function"""
+
+    def test_empty_list(self):
+        """Empty list"""
+        empty = []
+        self.assertEqual(max_integer(empty), None)
+    
+    def test_max_at_the_end(self):
+        """List where max value is the end"""
+        max_at_the_end = [1, 3, 2, 4]
+        self.assertEqual(max_integer(max_at_the_end), 4)
+
+    def test_max_at_begginning(self):
+        """List where max value is beginning"""
+        max_at_beginning = [4, 1, 3, 2]
+        self.assertEqual(max_integer(max_at_beginning), 4)
+
+    def test_ordered_list(self):
+        """Ordered list of integers"""
+        ordered = [1, 2, 3, 4]
+        self.assertEqual(max_integer(ordered), 4)
+
+    def test_unordered_list(self):
+        """Unordered list of integers"""
+        unordered = [1, 2, 4, 3]
+        self.assertEqual(max_integer(unordered), 4)
+
+    def test_unique_element_list(self):
+        """Single element list"""
+        unique_element = [40]
+        self.assertEqual(max_integer(unique_element), 40)
+
+    def test_ints_and_floats_list(self):
+        """List of ints and floats"""
+        ints_and_floats = [1.53, 15.5, -9, 15, 6]
+        self.assertEqual(max_integer(ints_and_floats), 15.5)
+
+    def test_float_list(self):
+        """List of floats"""
+        float = [1.53, 6.33, -9.123, 15.2, 6.0]
+        self.assertEqual(max_integer(float), 15.2)
+
+    def test_string_list(self):
+        """String"""
+        string = "vinGentz"
+        self.assertEqual(max_integer(string), 'y')
+
+    def test_neg_float_list(self):
+        """Negative floats"""
+        neg_float = [-5.55, -66.66, -111.1]
+        self.assertEqual(max_integer(neg_float), -5.55)
+
+    def test_diff_datatypes(self):
+        """Various data types"""
+        with self.assertRaises(TypeError):
+            max_integer([1, "1"])
+
+    def test_list_list(self):
+        """Nested list"""
+        list = [[1, 2], [2, 1]]
+        self.assertEqual(max_integer(list), [2, 1])
+        
+
+if __name__ == "__main__":
+    unitest.main()
