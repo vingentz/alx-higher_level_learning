aaf5a0a122eff795402435415aec1db5d3c710ff
Kevin Kariuki <vingentz@gmail.com>
2023-07-11 16:48:06 +0300

    Class Student with first name, Last name and age defined by 9-student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Class Student"""
+
+
+class Student:
+    """Defines class Student"""
+    def __init__(self, first_name, last_name, age):
+        """initializes Student class attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        """retrieves dictionary representation of Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            k = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    k[attr] = getattr(self, attr)
+            return k
