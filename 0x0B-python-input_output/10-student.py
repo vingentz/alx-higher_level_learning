ed5c69ff87ad9ae2aabf38095df0dee262996806
Kevin Kariuki <vingentz@gmail.com>
2023-07-11 16:50:09 +0300

    Class Student with first name, Last name and age defined by 9-student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,7 +11,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attr=None):
+    def to_json(self, attrs=None):
         """retrieves dictionary representation of Student instance"""
         if attrs is None:
             return self.__dict__
