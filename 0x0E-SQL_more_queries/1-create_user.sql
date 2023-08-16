572e0919fe2e9ecfdca2b6cfc838dee0d700f50b
Kevin Kariuki <vingentz@gmail.com>
2023-08-16 14:36:51 +0300

    Create user 1 with all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- create user_0d_1 with all privileges, password is user_0d_1
+-- if exists, script shouldnt fail
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
