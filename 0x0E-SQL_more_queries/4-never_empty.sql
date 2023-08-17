ce1ee9468ca7c68bf0fcba7863aa9834ae99d2fe
Kevin Kariuki <vingentz@gmail.com>
2023-08-17 16:47:28 +0300

    Create table id_not_null

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,7 @@
+-- creates the table id_not_null
+-- id_not_null description id INT with default value 1 and name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS id_not_null(
+	id INT DEFAULT 1,
+	name VARCHAR(256)
+	);
